eebbd8dd59d4e0e6e450e79c78056d5d040ceb4a
Zach Kessler <thatstickysmell@gmail.com>
2019-04-15 03:06:08 -0600

    Final work for homework 5

--- hm-5/sketch.js
+++ hm-5/sketch.js
@@ -1,46 +1,35 @@
 function setup() {
    createCanvas(windowWidth, windowHeight);
-   frameRate(20);
+   frameRate(20); // Change regular frame rate
+   noCursor(); // remove cursor
 }
 
 let bodyW = 40 // setting variable where bodyW stands for Body Width
-let armSwingAngle = 0 
+let armSwingAngle = 0;
 
 function draw() {
     background( 75);
-
-   push();
-    let squareX = windowWidth/2
-    let squareY = windowHeight/2
+    
+     // Sandbox the yellow square
+    push(); 
     stroke( 213, 243, 15);
     strokeWeight(3);
     fill( 'yellow'); 
+    // Place square origin
     rect( windowWidth/2, windowHeight/2, 20, 20);
-    squareX = squareX + 1;
-    squareY = squareY + 1;
+    // Erase and redraw the yellow square for every draw loop
+    // over one pixel and down two pixels
+    windowWidth = windowWidth + 1
+    windowHeight = windowHeight + 2
     pop();
 
+    // Create movement for the right arm
      armSwingAngle = armSwingAngle + 10
-    // Creating a sandbox for the person.
+
+    // Creating a sandbox for the person
     push();
     // Make the person follow the cursor
     translate( mouseX, mouseY) 
-
-    // Arms
-    strokeWeight( 10);
-    // Right Arm
-    push();
-    translate( bodyW, 35);
-    rotate( radians (armSwingAngle) ); 
-    line( -5, 0, 125, 35);
-    pop();
-    // Left Arm
-    push();
-    translate( -bodyW, 35);
-    rotate(radians (mouseX + mouseY) );
-    line( 5, 0, -125, 35);
-    pop();
-    
     
     // Legs
     strokeWeight( 3);
@@ -52,6 +41,40 @@ function draw() {
     // Body
     noStroke();
     fill( 0, 255, 0);
-    rect( -40, -25, 80, 190);
+    rect( -bodyW, -25, bodyW * 2, 190);
+
+    // Head
+    push();
+    strokeWeight(10);
+    stroke( 0, 0, 0);
+    fill( 255, 255, 255,);
+    ellipse( 0, -40, 100); 
+    // Eyes
+    strokeWeight(10);
+    point( -20, -60);
+    point( 20, -60);
+    // Mouth
+    strokeWeight(10);
+    fill( 'red');
+    quad( -20, -30, 20, -30, 18, 22, -18, 22);
+
+    // Arms
+    strokeWeight( 10);
+    // Right Arm
+    push();
+    // Using the translate function and variables to draw arms in relation to the body
+    translate( bodyW, 35);
+    // Apply swinging arm variable to the line
+    rotate( radians (armSwingAngle) ); 
+    line( -5, 0, 125, 35);
+    pop();
+    // Left Arm
+    push();
+    translate( -bodyW, 35);
+    // Allowing the left arm to move based on how you
+    // move the "cursor"
+    rotate(radians (mouseX + -mouseY) );
+    line( 5, 0, -125, 35);
+    pop();
     pop();
 }
